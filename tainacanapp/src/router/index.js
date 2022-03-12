b7ff8b0700dd61225072338a9356fb835df52002
joycitta-siqueira <joycitta@gmail.com>
2022-03-12 16:17:46 -0300

    atualização no index.js

--- tainacanapp/src/router/index.js
+++ tainacanapp/src/router/index.js
@@ -1,16 +1,11 @@
 import { createRouter, createWebHistory } from '@ionic/vue-router';
-import HomePage from '../views/HomePage.vue'
 
 const routes = [
   {
     path: '/',
     redirect: '/home'
   },
-  {
-    path: '/home',
-    name: 'Home',
-    component: HomePage
-  }
+
 ]
 
 const router = createRouter({
